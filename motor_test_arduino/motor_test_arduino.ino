ccd7885c654af09ef21c55c3d86ca23c66f7690a
Sam Baker <Sam.Baker101@gmail.com>
2022-05-31 16:29:32 -0400

    Update motor_test_arduino.ino
    Modified test script to align with prototype

--- motor_test_arduino/motor_test_arduino.ino
+++ motor_test_arduino/motor_test_arduino.ino
@@ -1,25 +1,33 @@
 //Sam Baker - 05/23/2022
 //Motor test code for Arduino using L298 motor controller and salvaged DC motors
 
-#define DUTY 90
+#define YDUTY 100
+#define XDUTY 60
+
+#define XPOS 5
+#define XNEG 4
+
+#define YPOS 6
+#define YNEG 7
 
 void setup() {
   //Feedback
   Serial.begin(115200);
   
   //Motor Control to L298 motor controller
-  pinMode(7, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(YPOS, OUTPUT);
+  pinMode(YNEG, OUTPUT);
+  pinMode(XPOS, OUTPUT);
+  pinMode(XNEG, OUTPUT);
 
   //PWM outputs
-  int set_duty = (255*DUTY)/100;
+  int set_y_duty = (255*YDUTY)/100;
+  int set_x_duty = (255*XDUTY)/100;
   
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
-  analogWrite(11, set_duty);
-  analogWrite(10, set_duty);
+  analogWrite(11, set_y_duty);
+  analogWrite(10, set_x_duty);
   
   //User Inputs
   pinMode(3, INPUT);
@@ -29,13 +37,10 @@ void setup() {
 }
 
 void loop() {
-  int test = digitalRead(2);
   digitalWrite(7, digitalRead(3));
-  digitalWrite(6, test);
-  digitalWrite(5, digitalRead(8));
-  digitalWrite(4, digitalRead(9));
-
-  Serial.println(test, DEC);
+  digitalWrite(6, digitalRead(2));
+  digitalWrite(XPOS, digitalRead(8));
+  digitalWrite(XNEG, digitalRead(9));
 
   delay(1);
 }
