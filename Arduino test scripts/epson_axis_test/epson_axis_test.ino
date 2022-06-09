aa79b9e0906933070dec49161b6d2be696f2f9f9
Sam Baker <Sam.Baker101@gmail.com>
2022-06-09 15:51:26 -0400

    Update epson_axis_test.ino
    motor test complete
    Distance value drifts at far end of axis

--- Arduino test scripts/epson_axis_test/epson_axis_test.ino
+++ Arduino test scripts/epson_axis_test/epson_axis_test.ino
@@ -11,6 +11,12 @@
 #define MOTOR_POS 10
 #define MOTOR_NEG 9
 
+#define BUTTON_NEG 6
+#define BUTTON_POS 5
+
+#define POS 1
+#define NEG 0
+
 //This is not suuuper accurate but will work for now
 #define DMULTIPLIER 10.0/736
 
@@ -19,6 +25,8 @@
 //counts posedges from encoder
 int enc_count = 0;
 int last_enc_count = 0;
+
+int motor_dir = 0;
 
 //(cm)
 float distance = 0.0;
@@ -31,6 +39,9 @@ void setup() {
   pinMode(MOTOR_PWM, OUTPUT);
   pinMode(MOTOR_POS, OUTPUT);
   pinMode(MOTOR_NEG, OUTPUT);
+
+  pinMode(BUTTON_POS, INPUT);
+  pinMode(BUTTON_NEG, INPUT);
   
   //Input from encoder (Blue wire from carrage)
   pinMode(ENC_I, INPUT);
@@ -45,6 +56,22 @@ void setup() {
 
 
 void loop() {
+  if (digitalRead(BUTTON_POS)){
+    motor_dir = POS;
+    digitalWrite(MOTOR_POS, HIGH);
+    }
+   else{
+    digitalWrite(MOTOR_POS, LOW);
+    }
+
+  if (digitalRead(BUTTON_NEG)){
+    motor_dir = NEG;
+    digitalWrite(MOTOR_NEG, HIGH);
+    }
+   else{
+    digitalWrite(MOTOR_NEG, LOW);
+    }
+  
   distance = enc_count * DMULTIPLIER;
   Serial.println(distance);
   delay(100);
@@ -54,7 +81,8 @@ void loop() {
 
 //increment enc_counter when input from encoder goes high
 void enc_ISR(){
-  enc_count ++;
+  if(motor_dir == POS) enc_count ++;
+  else if (motor_dir == NEG) enc_count --;
   }
 
 
@@ -64,6 +92,7 @@ void home_axis(){
     last_enc_count = enc_count;
     delay(1500);
     }while(last_enc_count != enc_count);
-
+    
     digitalWrite(MOTOR_NEG, LOW);
+    enc_count = 0;
   }  
