7e07afeba433bd8ea6a7fc299facb3a5e806666d
Sam Baker <Sam.Baker101@gmail.com>
2022-06-24 01:20:07 -0400

    Update stepper_test.ino

--- Arduino test scripts/stepper_test/stepper_test/stepper_test.ino
+++ Arduino test scripts/stepper_test/stepper_test/stepper_test.ino
@@ -3,18 +3,35 @@
 
 #define WIRE_ONE 6
 #define WIRE_TWO 5
-
+#define WIRE_THREE 4
+#define WIRE_FOUR 3
 
 void setup() {
   pinMode(WIRE_ONE, OUTPUT);
   pinMode(WIRE_TWO, OUTPUT);
+  pinMode(WIRE_THREE, OUTPUT);
+  pinMode(WIRE_FOUR, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(WIRE_TWO, LOW);
+  digitalWrite(WIRE_THREE, LOW);
+  digitalWrite(WIRE_FOUR, LOW);
+  digitalWrite(WIRE_ONE, HIGH);
+  delay(5);
   digitalWrite(WIRE_ONE, LOW);
+  digitalWrite(WIRE_THREE, LOW);
+  digitalWrite(WIRE_FOUR, LOW);
   digitalWrite(WIRE_TWO, HIGH);
   delay(5);
+  digitalWrite(WIRE_ONE, LOW);
   digitalWrite(WIRE_TWO, LOW);
-  digitalWrite(WIRE_ONE, HIGH);
+  digitalWrite(WIRE_FOUR, LOW);
+  digitalWrite(WIRE_THREE, HIGH);
+  delay(5);
+  digitalWrite(WIRE_ONE, LOW);
+  digitalWrite(WIRE_TWO, LOW);
+  digitalWrite(WIRE_THREE, LOW);
+  digitalWrite(WIRE_FOUR, HIGH);
   delay(5);
 }
