e353d4d0848e2a41362c9f34767086efdc239e9d
Sam Baker <Sam.Baker101@gmail.com>
2022-06-24 10:46:02 -0400

    Update stepper_test.ino

--- Arduino test scripts/stepper_test/stepper_test/stepper_test.ino
+++ Arduino test scripts/stepper_test/stepper_test/stepper_test.ino
@@ -1,37 +1,59 @@
 //Sam Baker - 05/23/2022
 //Naive test for salvaged stepper from Epson scanner bed
+//This exists primarily to find out the wiring/coil order of unmarked stepper
 
 #define WIRE_ONE 6
 #define WIRE_TWO 5
 #define WIRE_THREE 4
 #define WIRE_FOUR 3
 
+#define BUTTON 2
+
+//Coil states
+int state = 0;
+
+
 void setup() {
   pinMode(WIRE_ONE, OUTPUT);
   pinMode(WIRE_TWO, OUTPUT);
   pinMode(WIRE_THREE, OUTPUT);
   pinMode(WIRE_FOUR, OUTPUT);
+
+  pinMode(BUTTON, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON), button_ISR, RISING);
 }
 
 void loop() {
-  digitalWrite(WIRE_TWO, LOW);
-  digitalWrite(WIRE_THREE, LOW);
-  digitalWrite(WIRE_FOUR, LOW);
-  digitalWrite(WIRE_ONE, HIGH);
-  delay(5);
-  digitalWrite(WIRE_ONE, LOW);
-  digitalWrite(WIRE_THREE, LOW);
-  digitalWrite(WIRE_FOUR, LOW);
-  digitalWrite(WIRE_TWO, HIGH);
-  delay(5);
-  digitalWrite(WIRE_ONE, LOW);
-  digitalWrite(WIRE_TWO, LOW);
-  digitalWrite(WIRE_FOUR, LOW);
-  digitalWrite(WIRE_THREE, HIGH);
-  delay(5);
-  digitalWrite(WIRE_ONE, LOW);
-  digitalWrite(WIRE_TWO, LOW);
-  digitalWrite(WIRE_THREE, LOW);
-  digitalWrite(WIRE_FOUR, HIGH);
-  delay(5);
+  if (state == 0){
+    digitalWrite(WIRE_TWO, LOW);
+    digitalWrite(WIRE_THREE, LOW);
+    digitalWrite(WIRE_FOUR, LOW);
+    digitalWrite(WIRE_ONE, HIGH);
+    }
+
+  else if (state == 1){
+    digitalWrite(WIRE_ONE, LOW);
+    digitalWrite(WIRE_THREE, LOW);
+    digitalWrite(WIRE_FOUR, LOW);
+    digitalWrite(WIRE_TWO, HIGH);
+    }
+
+  else if (state == 2){ 
+    digitalWrite(WIRE_ONE, LOW);
+    digitalWrite(WIRE_TWO, LOW);
+    digitalWrite(WIRE_FOUR, LOW);
+    digitalWrite(WIRE_THREE, HIGH);
+    }
+
+  else if (state == 3){
+    digitalWrite(WIRE_ONE, LOW);
+    digitalWrite(WIRE_TWO, LOW);
+    digitalWrite(WIRE_THREE, LOW);
+    digitalWrite(WIRE_FOUR, HIGH);
+    }
 }
+
+void button_ISR(){
+  state = (state+1)%2;
+  }
