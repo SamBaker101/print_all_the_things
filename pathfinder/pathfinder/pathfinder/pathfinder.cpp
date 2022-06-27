bd8d95a0f0a7133bbcf5aa0668a7451c51713777
Sam Baker <Sam.Baker101@gmail.com>
2022-06-27 14:31:01 -0400

    Began pathfinder application

---
+++ pathfinder/pathfinder/pathfinder/pathfinder.cpp
@@ -0,0 +1,17 @@
+/*
+Sam Baker
+06/27/2022
+Pathfinder application for use with 3D printer salvage project - to be used with 2D drawing prototype
+When complete application will take in image file showing path to be cutout and return a set of vectors 
+to guide the print head around the path
+*/
+
+
+#include "pathfinder.h"
+
+int main() {
+	std::cout << "Hello World \n";
+
+	getchar();
+	return 0;
+}
