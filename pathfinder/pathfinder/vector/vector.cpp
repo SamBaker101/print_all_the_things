a632877467185034077a34c4b96966960991f315
Sam Baker <Sam.Baker101@gmail.com>
2022-07-01 14:32:59 -0400

    modify Vector module

--- pathfinder/pathfinder/vector/vector.cpp
+++ pathfinder/pathfinder/vector/vector.cpp
@@ -6,6 +6,13 @@ Vector module intended to draw vectors between points along path
 
 #include "vector.hpp"
 
+const Scalar_t Vector::get_start_x() const  { return start_x; }
+const Scalar_t Vector::get_start_y() const  { return start_y; }
+const Scalar_t Vector::get_end_x()   const  { return end_x; }
+const Scalar_t Vector::get_end_y()   const  { return end_y; }
+const Scalar_t Vector::get_len_x()   const  { return len_x; }
+const Scalar_t Vector::get_len_y()   const  { return len_y; }
+
 
 void Vector::set_start(Scalar_t x, Scalar_t y) {
 	start_x = x;
@@ -30,4 +37,16 @@ Scalar_t Vector::get_length() {
 
 Scalar_t Vector::get_angle() {
 	return tan(len_y / len_x);
+}
+
+Vector operator+(const Vector v1, const Vector v2) {
+	return Vector(v1.get_start_x(), v1.get_start_y(),
+				  v1.get_end_x() + v2.get_len_x(), 
+				  v1.get_end_y() + v2.get_len_y());
+}
+
+Vector operator-(const Vector v1, const Vector v2) {
+	return Vector(v1.get_start_x(), v1.get_start_y(), 
+				  v1.get_end_x() - v2.get_len_x(), 
+				  v1.get_end_y() - v2.get_len_y());
 }
--- pathfinder/pathfinder/vector/vector.hpp
+++ pathfinder/pathfinder/vector/vector.hpp
@@ -31,18 +31,30 @@ public:
 		len_x = -1;
 	}
 
-	Vector(Scalar_t start_a, Scalar_t start_b, Scalar_t end_a, Scalar_t end_b) {
-		start_x = start_a;
-		start_y = start_b;
-		end_x = end_a;
-		end_y = end_b;
+	Vector(Scalar_t start_x_i, Scalar_t start_y_i, Scalar_t end_x_i, Scalar_t end_y_i) {
+		start_x = start_x_i;
+		start_y = start_y_i;
+		end_x = end_x_i;
+		end_y = end_y_i;
 		len_y = end_y - start_y;
 		len_x = end_x - start_x;
 	}
+
+	const Scalar_t get_start_x() const;
+	const Scalar_t get_start_y() const;
+	const Scalar_t get_end_x() const;
+	const Scalar_t get_end_y() const;
+	const Scalar_t get_len_x() const;
+	const Scalar_t get_len_y() const;
+
 
 	void set_start(Scalar_t x, Scalar_t y);
 	void set_end(Scalar_t x, Scalar_t y);
 	void print_vector();
 	Scalar_t get_length();
 	Scalar_t get_angle();
+
 };
+
+Vector operator+(const Vector v1, const Vector v2);
+Vector operator-(const Vector v1, const Vector v2);
